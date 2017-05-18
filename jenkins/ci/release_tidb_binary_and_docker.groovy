be6f3c43d62cb67f846a294756bcf87c0d583b66
iamxy <kenneth.liu.xy@gmail.com>
2017-05-18 19:15:50 +0800

    jenkins: add release_tidb_binary_and_docker.groovy

---
+++ jenkins/ci/release_tidb_binary_and_docker.groovy
@@ -0,0 +1,160 @@
+def call(TIDB_BRANCH, TIKV_BRANCH, PD_BRANCH, RELEASE_TAG) {
+
+    def UCLOUD_OSS_URL = "http://pingcap-dev.hk.ufileos.com"
+    env.PATH = "/home/jenkins/bin:/bin:${env.PATH}"
+    def nodename = "${env.NODE_NAME}"
+    def HOSTIP = nodename.getAt(7..(nodename.lastIndexOf('-') - 1))
+    def tidb_sha1, tikv_sha1, pd_sha1
+
+    catchError {
+        node('delivery') {
+            stage('Prepare') {
+                deleteDir()
+
+                dir ('centos7') {
+                    tidb_sha1 = sh(returnStdout: true, script: "curl ${UCLOUD_OSS_URL}/refs/pingcap/tidb/${TIDB_BRANCH}/centos7/sha1").trim()
+                    sh "curl ${UCLOUD_OSS_URL}/builds/pingcap/tidb/${tidb_sha1}/centos7/tidb-server.tar.gz | tar xz"
+
+                    tikv_sha1 = sh(returnStdout: true, script: "curl ${UCLOUD_OSS_URL}/refs/pingcap/tikv/${TIKV_BRANCH}/centos7/sha1").trim()
+                    sh "curl ${UCLOUD_OSS_URL}/builds/pingcap/tikv/${tikv_sha1}/centos7/tikv-server.tar.gz | tar xz"
+
+                    pd_sha1 = sh(returnStdout: true, script: "curl ${UCLOUD_OSS_URL}/refs/pingcap/pd/${PD_BRANCH}/centos7/sha1").trim()
+                    sh "curl ${UCLOUD_OSS_URL}/builds/pingcap/pd/${pd_sha1}/centos7/pd-server.tar.gz | tar xz"
+                }
+
+                dir ('centos6') {
+                    def tikv_centos6_sha1 = sh(returnStdout: true, script: "curl ${UCLOUD_OSS_URL}/refs/pingcap/tikv/${TIKV_BRANCH}/centos6/sha1").trim()
+                    sh "curl ${UCLOUD_OSS_URL}/builds/pingcap/tikv/${tikv_centos6_sha1}/centos6/tikv-server.tar.gz | tar xz"
+                }
+            }
+
+            stage('Push Centos7 Binary') {
+                def target = "tidb-${RELEASE_TAG}-linux-amd64"
+
+                dir("${target}") {
+                    sh "cp -R ../centos7/bin ./"
+                }
+
+                sh """
+                tar czvf ${target}.tar.gz ${target}
+                sha256sum ${target}.tar.gz > ${target}.sha256
+                md5sum ${target}.tar.gz > ${target}.md5
+                """
+
+                sh """
+                upload.py ${target}.tar.gz ${target}.tar.gz
+                upload.py ${target}.sha256 ${target}.sha256
+                upload.py ${target}.md5 ${target}.md5
+                """
+            }
+
+            stage('Push Centos6 Binary') {
+                def target = "tidb-${RELEASE_TAG}-linux-amd64-centos6"
+
+                dir("${target}") {
+                    sh "cp -R ../centos7/bin ./"
+                    sh "cp ../centos6/bin/tikv-server bin/"
+                }
+
+                sh """
+                tar czvf ${target}.tar.gz ${target}
+                sha256sum ${target}.tar.gz > ${target}.sha256
+                md5sum ${target}.tar.gz > ${target}.md5
+                """
+
+                sh """
+                upload.py ${target}.tar.gz ${target}.tar.gz
+                upload.py ${target}.sha256 ${target}.sha256
+                upload.py ${target}.md5 ${target}.md5
+                """
+            }
+
+            stage('Push tidb Docker') {
+                dir('tidb_docker_build') {
+                    sh  """
+                    cp ../centos7/bin/tidb-server ./
+                    cat > Dockerfile << __EOF__
+FROM pingcap/alpine-glibc
+COPY tidb-server /tidb-server
+EXPOSE 4000
+ENTRYPOINT ["/tidb-server"]
+__EOF__
+                    """
+                }
+
+                withDockerServer([uri: "tcp://${HOSTIP}:32376"]) {
+                    docker.build("pingcap/tidb:${RELEASE_TAG}", "tidb_docker_build").push()
+                }
+            }
+
+            stage('Push tikv Docker') {
+                dir('tikv_docker_build') {
+                    sh """
+                    cp ../centos7/bin/tikv-server ./
+                    cat > Dockerfile << __EOF__
+FROM pingcap/alpine-glibc
+ENV TZ /etc/localtime
+COPY tikv-server /tikv-server
+EXPOSE 20160
+ENTRYPOINT ["/tikv-server"]
+__EOF__
+                    """
+                }
+
+                withDockerServer([uri: "tcp://${HOSTIP}:32376"]) {
+                    docker.build("pingcap/tikv:${RELEASE_TAG}", "tikv_docker_build").push()
+                }
+            }
+
+            stage('Push pd Docker') {
+                dir('pd_docker_build') {
+                    sh """
+                    cp ../centos7/bin/pd-server ./
+                    cp ../centos7/bin/pd-ctl ./
+                    cat > Dockerfile << __EOF__
+FROM pingcap/alpine-glibc
+COPY pd-server /pd-server
+COPY pd-ctl /pd-ctl
+EXPOSE 2379 2380
+ENTRYPOINT ["/pd-server"]
+__EOF__
+                    """
+                }
+
+                withDockerServer([uri: "tcp://${HOSTIP}:32376"]) {
+                    docker.build("pingcap/pd:${RELEASE_TAG}", "pd_docker_build").push()
+                }
+            }
+        }
+
+        currentBuild.result = "SUCCESS"
+    }
+
+    stage('Summary') {
+        def duration = ((System.currentTimeMillis() - currentBuild.startTimeInMillis) / 1000 / 60).setScale(2, BigDecimal.ROUND_HALF_UP)
+        def slackmsg = "[${env.JOB_NAME}-${env.BUILD_NUMBER}] `${currentBuild.result}`" + "\n" +
+        "Elapsed Time: `${duration}` Mins" + "\n" +
+        "tidb branch: `${TIDB_BRANCH}`, githash: `${tidb_sha1.take(7)}`" + "\n" +
+        "tikv branch: `${TIKV_BRANCH}`, githash: `${tikv_sha1.take(7)}`" + "\n" +
+        "pd   branch: `${PD_BRANCH}`, githash: `${pd_sha1.take(7)}`" + "\n" +
+        "TiDB Binary download URL:" + "\n" +
+        "http://download.pingcap.org/tidb-${RELEASE_TAG}-linux-amd64.tar.gz" + "\n" +
+        "TiDB Binary sha256   URL:" + "\n" +
+        "http://download.pingcap.org/tidb-${RELEASE_TAG}-linux-amd64.sha256" + "\n" +
+        "TiDB Binary (for Centos6) download URL:" + "\n" +
+        "http://download.pingcap.org/tidb-${RELEASE_TAG}-linux-amd64-centos6.tar.gz" + "\n" +
+        "TiDB Binary (for Centos6) sha256   URL:" + "\n" +
+        "http://download.pingcap.org/tidb-${RELEASE_TAG}-linux-amd64-centos6.sha256" + "\n" +
+        "tidb docker image: `pingcap/tidb:${RELEASE_TAG}`" + "\n" +
+        "tikv docker image: `pingcap/tikv:${RELEASE_TAG}`" + "\n" +
+        "pd   docker image: `pingcap/pd:${RELEASE_TAG}`"
+
+        if (currentBuild.result != "SUCCESS") {
+            slackSend channel: '#binary_publish', color: 'danger', teamDomain: 'pingcap', tokenCredentialId: 'slack-pingcap-token', message: "${slackmsg}"
+        } else {
+            slackSend channel: '#binary_publish', color: 'good', teamDomain: 'pingcap', tokenCredentialId: 'slack-pingcap-token', message: "${slackmsg}"
+        }
+    }
+}
+
+return this
